454d2a1fca00edbfc7f8f04c7f4eb03a6d0cd6d5
AraReyes <34664461+AraReyes@users.noreply.github.com>
2017-12-18 17:17:49 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+
+<html>
+	<header>
+		<title>ReptilesCiencias</title>
+		<meta charset="utf-8">
+		<link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet"> 
+
+		<style type="text/css">
+			@import url('https://fonts.googleapis.com/css?family=Bungee|Fjalla+One|Indie+Flower|Lobster|Shadows+Into+Light');
+			
+			body {
+				background-image: url("imagenes/fondo.jpg");
+				margin:0; 
+			}
+			
+			#cabecera{
+				background-image: url("imagenes/camaleones.jpg");
+				color: white;
+				width: 1000px;
+				height: 450px;
+				background-size: 1000px 500px;
+				margin: 0 auto 0 auto;
+				font-family: 'Fjalla One';
+				text-shadow: 2px 2px #000000;
+				}
+			
+			#novedades{
+				width: 1000px;
+				font-family: 'Fjalla One';
+				color: white;
+				text-shadow: 2px 2px #000000;
+				}
+			
+			#imagen{
+				border-radius: 12px;
+				}	
+				
+			.topnav {
+				width: 1000px;
+				overflow: hidden;
+				background-color: #333;
+				}
+
+			.topnav a {
+			  float: left;
+			  color: #f2f2f2;
+			  text-align: center;
+			  padding: 14px 16px;
+			  text-decoration: none;
+			  font-size: 17px;
+				}
+
+			.topnav a:hover {
+			  background-color: #ddd;
+			  color: black;
+				}
+				
+				
+			.topnav .search-container {
+			  float: right;
+			}
+
+			.topnav input[type=text]{
+			  padding: 6px;
+			  margin-top: 8px;
+			  font-size: 17px;
+			  border: none;
+			}				
+			
+		</style>
+	</header>
+	<body>
+		<center>
+		<div class="topnav">
+		  <a class="active" href="index.php">Inicio</a>
+		  <a href="tienda.php">Tienda</a>
+		  <a href="#contact">Contacto</a>
+		  <a href="quienesSomos.php">Sobre nosotros</a>
+		</div>
+		
+		<div id="cabecera">
+			<br>
+			<p><h1>BIENVENIDO A REPTILES CIENCIAS</h1></p>
+			</center>
+		</div>
+		
+		<center>
+			<div id="novedades">
+			<p><h1>NOVEDADES<h1></p>
+			<hr color="#AB1616">
+		
+			<table>
+			<tr>
+				<img src="imagenes/nactus.jpg" width="850" height="250"><br>
+			</tr>
+			<tr>
+				
+				<img src="imagenes/bamboo.jpg" width="850" height="300">
+			</tr>
+			</table>
+			</center>
+		</div>
+	
+	</body>
+</html>
---
+++ quienesSomos.php
@@ -0,0 +1,167 @@
+
+<html>
+	<header>
+		<meta charset="utf-8">
+		<title>Quienes somos</title>
+			<style type="text/css">
+			@import url('https://fonts.googleapis.com/css?family=Bungee|Fjalla+One|Indie+Flower|Lobster|Shadows+Into+Light');
+			
+			div.polaroid {
+				width: 80%;
+				background-color: white;
+				box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			}
+
+			img {width: 100%}
+
+			div.container {
+				text-align: center;
+				padding: 10px 20px;
+			}
+							
+			.topnav {
+				overflow: hidden;
+				background-color: #333;
+				}
+
+			.topnav a {
+			  float: left;
+			  color: #f2f2f2;
+			  text-align: center;
+			  padding: 14px 16px;
+			  text-decoration: none;
+			  font-size: 17px;
+				}
+
+			.topnav a:hover {
+			  background-color: #ddd;
+			  color: black;
+			}
+
+			.topnav input[type=text]{
+			  padding: 6px;
+			  margin-top: 8px;
+			  font-size: 17px;
+			  border: none;
+			}
+	
+							
+			* {
+			box-sizing: border-box;
+			}
+
+			body {
+			margin-left: 150px; 
+			margin-right: 150px;
+			background-image: url("imagenes/green.jpg");
+			}
+
+			/* Style the header */
+			.header {
+				font-family: 'Lobster';
+				background-color: #B6C839;
+				padding: 40px;
+				text-align: center;
+				width: 1000px;
+			}
+
+			/* Create three equal columns that floats next to each other */
+			.column {
+				float: left;
+				width: 33.33%;
+				padding: 10px;
+				height: 300px;
+				font-faily: 'Fjalla One';
+			}
+
+			/* Clear floats after the columns */
+			.row:after {
+				content: "";
+				display: table;
+				clear: both;
+				
+				
+			}
+
+			/* Style the footer */
+			.footer {
+				background-color: #f1f1f1;
+				padding: 10px;
+				text-align: center;
+			}
+
+			/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+			@media (max-width: 600px) {
+				.column {
+					width: 80%;
+				}
+				
+			}			
+			
+		</style>
+	</header>
+	<body>
+		<center>
+		<div class="topnav">
+		  <a class="active" href="index.php">Inicio</a>
+		  <a href="tienda.php">Tienda</a>
+		  <a href="#contact">Contacto</a>
+		  <a href="quienesSomos.php">Sobre nosotros</a>
+		</div>
+
+
+		<div class="header">
+			<center>
+			<div class="polaroid">
+				<img src="imagenes/igui.jpg" alt="Norway" style="width:100%">
+			<div class="container">
+			<p>Iguana verde.</p>
+			</div>
+			</div>
+			</center>
+		  <h1>Acerca de nosotros</h1>
+		  <h3>La tienda de reptiles y fauna exótica mas grande le México.</h3>
+		  <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
+		  
+		  <h3>Todos nuestros ejemplares con garantía.</h3>
+		  <p>At vero eos et accusamus et iusto odio vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
+		
+		</div>
+
+		<center>
+		<div class="row">
+		  <div class="column" style="background-color:#B1E57C;"><h3><b>Calidad</b></h3>
+		  On equal blame et accusamus et iusto odio vero eos et accusamus belongs to those who fail in their duty through weakness of will,to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.
+		  </div>
+		  <div class="column" style="background-color:#BDF07A;"><h3><b>Atención</b></h3>
+		  On equal blame et accusamus et iusto odio vero eos et accusamus belongs to those who fail in their duty through weakness of will,to those through weakness of will,to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.
+		  </div>
+		  <div class="column" style="background-color:#BBFB67;"><h3><b>Cuidado</b></h3>
+		  On equal et accusamus et iusto odio vero eos et accusamus blame belongs to those who fail in their, to those who fail in their duty through weakness of will belongs to those who fail in their duty through weakness of will,to those who fail in their.
+		  </div>
+		</div>
+		</center>
+		
+		<div class="header">
+		  <h3>Actualizamos nuestra página cada semana.</h3>
+		  <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
+		  
+		  <h3>Tu información está protegida con nosotros.</h3>
+		  <p>At vero eos et accusamus et iusto odio vero eos et accusamus et iusto odio. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
+		
+		</div>
+		
+		<div class="footer">
+			Conoce más sobre nosotros en:
+		  <ul class="icons alt">
+				<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+				<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+				<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+		</ul>
+		</div>
+
+
+	</body>
+
+</html>
